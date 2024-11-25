6ebce08c42d93468b30c1b8f1657c62961aec4da
lewisl23 <lewisliu23@gmail.com>
2024-11-25 11:05:50 +0000

    Initialize Git

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Step 1: User input protein family and taxonomic group
 #Step 2: Obtain relevant protein sequence data
+
 #Step 3: Decision to check so that sequences are possibly below 1,000
 #Step 4: Do BLAST and obtain level of conservation (plot)
 #Step 5: Scan protein sequence with motifs (PROSITE databas)
